12c09e63de3b8737f7dd12eb5071ddc2c0946306
David Will <pfefferundsalz@posteo.de>
2023-03-11 10:58:35 +0100

    changed redirect to boards page

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -23,6 +23,6 @@ export const actions: Actions = {
                 console.log(err)
                 return fail(400, { message: "Could not log in user"})
             }
-            throw redirect(302, "/")
+            throw redirect(302, "/boards")
     }
 };
